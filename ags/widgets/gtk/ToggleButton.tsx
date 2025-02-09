a23320b6d233b489615fe58792f4e7f5bfc26b30
Max G. <66702601+s1idewhist1e@users.noreply.github.com>
2025-02-08 17:59:55 -0700

    AGS: Add default gtk widget `ToggleButton`

---
+++ ags/widgets/gtk/ToggleButton.tsx
@@ -0,0 +1,9 @@
+import GObject from "gi://GObject";
+import { astalify, type ConstructProps, Gtk, Gdk, Widget } from "astal/gtk4";
+
+export type ToggleButtonProps = ConstructProps<Gtk.ToggleButton, Gtk.ToggleButton.ConstructorProps>
+export const ToggleButton = astalify<Gtk.ToggleButton, Gtk.ToggleButton.ConstructorProps>(Gtk.ToggleButton, {
+  getChildren(self) { return [self.child] },
+  setChildren(self, children) { self.set_child(children[0]) },
+});
+
